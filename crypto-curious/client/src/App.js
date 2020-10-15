de4d15bcb42370d707ffc6b9a1bc210d93504875
Hussein Razian <husseinrazian@outlook.com>
2020-10-15 17:07:23 -0600

    Added Style Folders and Necessary Style Files

--- crypto-curious/client/src/App.js
+++ crypto-curious/client/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import fire from './fire';
 import LoginPage from './components/LoginPage/LoginPage';
 import MainPage from './components/MainPage/MainPage'
